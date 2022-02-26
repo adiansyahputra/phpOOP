0168fe54f3097ac6dd92a2d2a955304549d74f1b
Adiansyah Putra <adiansyahhputra@gmail.com>
2022-02-26 17:52:50 +0700

    menambah file baru

--- phpOOP.php
+++ phpOOP.php
@@ -1289,4 +1289,49 @@ var_dump($person);
 $arrayPerson = (array) $person;
 var_dump($arrayPerson);
 
+Object Iteration
+Saat kita membuat object dari sebuah class, kita bisa melakukan iterasi ke semua properties yang terdapat di object tersebut menggunakan foreach
+Hal ini mempermudah kita saat ingin mengakses semua properties yang ada di object
+Secara default, hanya properties yang public yang bisa diakses oleh foreach
+
+Kode : Object Iteration
+class Data implements IteratorAggregate
+{
+    var string $first = "First";
+    public string $second = "Second";
+    private string $third = "Third";
+    protected string $forth = "Forth";
+
+$data = new Data();
+
+foreach ($data as $property => $value) {
+    echo "$property : $value" . PHP_EOL;
+}
+
+Iterator
+Sebelumnya kita melakukan iterasi data di properties secara otomatis menggunakan foreach
+Jika kita ingin menangani hal ini secara manual, kita bisa menggunakan Iterator
+Iterator adalah interface yang digunakan untuk melakukan iterasi, namun membuat Iterator secara manual lumayan cukup ribet, oleh karena itu sekarang kita akan gunakan ArrayIterator, yaitu iterator yang menggunakan array sebagai data iterasi nya
+Dan agar class kita bisa di iterasi secara manual, kita bisa menggunakan interface IteratorAggregate, disana kita hanya butuh meng-override function getIterator() yang mengembalikan data Iterator
+
+Kode : Iterator
+class Data implements IteratorAggregate
+{
+    var string $first = "First";
+    public string $second = "Second";
+    private string $third = "Third";
+    protected string $forth = "Forth";
+
+    public function getIterator()
+    {
+        $array = [
+            "first" => $this->first,
+            "second" => $this->second,
+            "third" => $this->third,
+            "forth" => $this->forth,
+        ];
+    //
+        return new ArrayIterator($array);
+    }
+
 */
