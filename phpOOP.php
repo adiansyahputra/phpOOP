9ca5d673b2ed30d23a6d94ebd025e9a20be15f4a
Adiansyah Putra <adiansyahhputra@gmail.com>
2022-02-27 12:03:45 +0700

    add materi RegularExpression

--- phpOOP.php
+++ phpOOP.php
@@ -1932,5 +1932,38 @@ var_dump($exception->getTrace());
 
     echo $exception->getTraceAsString() . PHP_EOL;
 
+Regular Expression
+PHP mendukung regular expression yang kompatibel dengan bahasa pemrograman Perl
+Regular expression merupakan fitur yang digunakan untuk melakukan pencarian di string menggunakan pola tertentu
+Materi ini sebenarnya materi yang sangat panjang, sehingga disini kita akan bahas sedikit pengenalannya
+Untuk pembuatan pattern di regular expression, detailnya bisa dibaca disini : https://www.php.net/manual/en/pcre.pattern.php 
+
+Function Regular Expression
+ Function Regular Expression
+Keterangan
+preg_match ($pattern ,$subject)
+preg_match_all ($pattern ,$subject)
+Digunakan untuk mencari match pattern
+preg_replace ( $pattern , $replacement, $subject)
+Digunakan untuk mereplace semua pattern dengan replacement
+preg_split ( $pattern , $subject)
+Digunakan untuk memotong dengan pattern menjadi array
+
+Kode : Regular Expression Match
+$matches = [];
+$result = preg_match_all("/eko|awan|edy/i", "Eko Kurniawan Khannedy", $matches);
+
+var_dump($result);
+var_dump($matches);
+
+Kode : Regular Expression Replace
+$result = preg_replace("/anjing|bangsat/i", "***", "dasar lu ANJING dan BANGSAT!");
+
+var_dump($result);
+
+Kode : Regular Expression Split
+$result = preg_split("/[\s,-]/", "Eko Kurniawan Khannedy,Programmer,Zaman-Now");
+
+var_dump($result);
 
 */
