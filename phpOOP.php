92916d57255683664e9f8d2b89eb5dfba5cc6403
Adiansyah Putra <adiansyahhputra@gmail.com>
2022-02-27 11:11:27 +0700

    Materi Date Time

--- phpOOP.php
+++ phpOOP.php
@@ -1719,6 +1719,83 @@ class Dog extends Animal
     }
 }
 
+DateTime
+Biasanya dalam bahasa pemrograman sudah disediakan cara untuk memanipulasi data waktu, termasuk di PHP
+Di PHP, kita bisa menggunakan class DateTime untuk memanipulasi data waktu
+Ada banyak sekali function di class DateTime yang bisa kita gunakan untuk memanipulasi data waktu
+
+DateTime Function
+ DateTime Function
+Keterangan
+setTime($hour, $minute, $second)
+Mengubah waktu DateTime
+setDate($year, $month, $day)
+Mengubah tanggal DateTime
+setTimestamp($unixTimestamp)
+Mengubah unix timestamp DateTime
+
+Kode : DateTime
+$dateTime = new DateTime();
+$dateTime->setDate(1990, 1, 20);
+$dateTime->setTime(10, 10, 10, 0);
+
+var_dump($dateTime);
+
+DateInterval
+Kadang kita hanya ingin memanipulasi waktu dan tanggal sebagian saja, misal hanya menambah 1 tahun, atau mengurai beberapa hari
+Hal ini bisa dilakukan dengan function add milik DateTime
+Namun function add tersebut menerima parameter berupa DateInterval
+Saat menggunakan DateInterval duration, kita perlu menentukan berapa banyak kita menambah interval 
+Kita bisa lihat detailnya disini : https://www.php.net/manual/en/dateinterval.construct.php 
+Untuk pembuatan duration, harus diawali dengan karakter P yang artinya period
+
+Kode : DateInterval
+$dateTime = new DateTime();
+$dateTime->setDate(1990, 1, 20);
+$dateTime->setTime(10, 10, 10, 0);
+
+$dateTime->add(new DateInterval("P1Y"));
+
+$minusOneMonth = new DateInterval("P1M");
+$minusOneMonth->invert = true;
+$dateTime->add($minusOneMonth);
+
+var_dump($dateTime);
+
+DateTimeZone
+Saat kita membuat object DateTime, dia akan secara otomatis membuat waktu saat ini sesuai dengan timezone yang di setting di konfigurasi date.timezone di file php.ini
+Atau kita bisa menggunakan function setTimeZone untuk mengubah timezone DateTime
+
+Kode : DateTimeZone
+$now = new DateTime();
+var_dump($now);
+$now->setTimezone(new DateTimeZone("America/Toronto"));
+var_dump($now);
+
+Format DateTime
+Kadang kita ingin membuat representasi string dari DateTime yang sudah kita buat
+Hal ini bisa kita lakukan menggunakan function format()
+Function format() menerima argument berupa format string, ini bisa kita gunakan untuk memanipulasi cara kita menampilkan string format waktu
+Untuk detailnya kita bisa lihat di halaman dokumentasi resminya
+https://www.php.net/manual/en/datetime.format.php 
+
+Kode : Format DateTime
+$string = $now->format("Y-m-d H:i:s");
+echo "Waktu Saat Ini : $string" . PHP_EOL;
+
+Parse DateTime
+Selain format DateTime menjadi string, di PHP juga kita bisa melakukan hal sebaliknya, yaitu memparsing string menjadi DateTime sesuai dengan format yang kita mau
+Untuk melakukan itu, kita bisa menggunakan static function createFromFormat() dari class DateTime
+
+Kode : Parse DateTime
+$date = DateTime::createFromFormat("Y-m-d H:i:s", "2020-10-10 10:10:10", new DateTimeZone("Asia/Jakarta"));
+if ($date) {
+    var_dump($date);
+} else {
+    echo "Format Salah" . PHP_EOL;
+}
+
+
 
 
 */
