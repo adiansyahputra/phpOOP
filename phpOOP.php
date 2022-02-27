45e5b925a79e80dbf330c6708db5fa62a3b1a65c
Adiansyah Putra <adiansyahhputra@gmail.com>
2022-02-27 11:42:43 +0700

    Add Materi Exception

--- phpOOP.php
+++ phpOOP.php
@@ -1795,7 +1795,142 @@ if ($date) {
     echo "Format Salah" . PHP_EOL;
 }
 
+Exception
+Saat kita membuat aplikasi, kita tidak akan terhindar dengan yang namanya error
+Di PHP, error direpresentasikan dengan istilah exception, dan semua direpresentasikan dalam bentuk class exception
+Kita bisa menggunakan class exception sendiri, atau menggunakan yang sudah disediakan oleh PHP
+Jika kita ingin membuat exception, maka kita harus membuat class yang implement interface Throwable atau turunan-turunannya
 
+Kode : Membuat Class Exception
+class ValidationException extends Exception
+{
+}
+
+Membuat Exception
+Exception biasanya terjadi di function
+Di dalam kode program kita, untuk membuat exception kita cukup menggunakan kata kunci throw, diikuti dengan object exception nya
+
+Kode : Membuat Exception
+function validateLoginRequest(LoginRequest $request)
+{
+    if (!isset($request->username)) {
+        throw new ValidationException("Username is null");
+    } else if (!isset($request->password)) {
+        throw new ValidationException("Password is null");
+    } else if (trim($request->username) == "") {
+        throw new Exception("Username is empty");
+    } else if (trim($request->password) == "") {
+        throw new Exception("Password is empty");
+    }
+}
+
+Try Catch
+Saat kita memanggil sebuah function yang bisa menyebabkan exception, maka kita disarankan menggunakan try-catch expression di PHP
+Ini gunanya agar kita bisa menangkap exception yang terjadi, karena jika tidak ditangkap, lalu terjadi exception, maka secara otomatis program kita akan berhenti
+Cara menggunakan try-catch expression di PHP sangat mudah, di block try, kita tinggal panggil method yang bisa menyebabkan exception, dan di block catch, kita bisa melakukan sesuatu jika terjadi exception
+
+Kode : Try Catch
+require_once "exception/ValidationException.php";
+require_once "data/LoginRequest.php";
+require_once "helper/Validation.php";
+
+$loginRequest = new LoginRequest();
+$loginRequest->username = "  ";
+$loginRequest->password = "  ";
+
+try {
+    validateLoginRequest($loginRequest);
+    echo "VALID" . PHP_EOL;
+} catch (ValidationException | Exception $exception) {
+    echo "Error : {$exception->getMessage()}" . PHP_EOL;
+
+    var_dump($exception->getTrace());
+
+    echo $exception->getTraceAsString() . PHP_EOL;
+} finally {
+    echo "ERROR ATAU ENGGAK, AKAN DIEKSEKUSI" . PHP_EOL;
+}
+
+Kode : Multiple Try Catch (1)
+require_once "exception/ValidationException.php";
+require_once "data/LoginRequest.php";
+require_once "helper/Validation.php";
+
+$loginRequest = new LoginRequest();
+$loginRequest->username = "  ";
+$loginRequest->password = "  ";
+
+try {
+    validateLoginRequest($loginRequest);
+    echo "VALID" . PHP_EOL;
+} catch (ValidationException | Exception $exception) {
+    echo "Error : {$exception->getMessage()}" . PHP_EOL;
+
+    var_dump($exception->getTrace());
+
+    echo $exception->getTraceAsString() . PHP_EOL;
+} finally {
+    echo "ERROR ATAU ENGGAK, AKAN DIEKSEKUSI" . PHP_EOL;
+}
+
+Kode : Multiple Try Catch (2)
+require_once "exception/ValidationException.php";
+require_once "data/LoginRequest.php";
+require_once "helper/Validation.php";
+
+$loginRequest = new LoginRequest();
+$loginRequest->username = "  ";
+$loginRequest->password = "  ";
+
+try {
+    validateLoginRequest($loginRequest);
+    echo "VALID" . PHP_EOL;
+} catch (ValidationException | Exception $exception) {
+    echo "Error : {$exception->getMessage()}" . PHP_EOL;
+
+    var_dump($exception->getTrace());
+
+    echo $exception->getTraceAsString() . PHP_EOL;
+} finally {
+    echo "ERROR ATAU ENGGAK, AKAN DIEKSEKUSI" . PHP_EOL;
+}
+
+Finally Keyword
+Dalam try-catch, kita bisa menambahkan block finally
+Block finally ini adalah block dimana akan selalu dieksekusi baik terjadi exception ataupun tidak
+Ini sangat cocok ketika kita ingin melakukan sesuatu, tidak peduli sukses ataupun gagal, misal di block try kita ingin membaca file, di block catch kita akan tangkap jika terjadi error, dan di block finally error ataupun sukses membaca file, kita wajib menutup koneksi ke file tersebut, biar tidak menggantung di memory
+
+Kode : Finally
+require_once "exception/ValidationException.php";
+require_once "data/LoginRequest.php";
+require_once "helper/Validation.php";
+
+$loginRequest = new LoginRequest();
+$loginRequest->username = "  ";
+$loginRequest->password = "  ";
+
+try {
+    validateLoginRequest($loginRequest);
+    echo "VALID" . PHP_EOL;
+} catch (ValidationException | Exception $exception) {
+    echo "Error : {$exception->getMessage()}" . PHP_EOL;
+
+    var_dump($exception->getTrace());
+
+    echo $exception->getTraceAsString() . PHP_EOL;
+} finally {
+    echo "ERROR ATAU ENGGAK, AKAN DIEKSEKUSI" . PHP_EOL;
+}
+
+Debug Exception
+Exception di PHP memiliki sebuah function bernama getTrace()
+Function getTrace() berisikan informasi dari exception yang terjadi, seperti lokasi file, baris ke berapa, function mana sampai argumenty yang dikirim di function tersebut apa
+Ini sangat cocok untuk kita jika ingin mendebug ketika terjadi exception
+
+Kode : Debug Exception
+var_dump($exception->getTrace());
+
+    echo $exception->getTraceAsString() . PHP_EOL;
 
 
 */
